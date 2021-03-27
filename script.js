14e4fb271908b8193f2865eae7d696341b7401f9
etominskdetka <9798dima@gmail.com>
2021-03-27 23:16:17 +0300

    feat: add file script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/*var age = 25;
+var name = "Owen";
+
+if (age > 14) {
+  alert("Sorry, this page for kids only!");
+} else {
+  alert("Welcome " + name + "!");
+}*/
+
+
+var winners = 2;
+var name = "Duke";
+var isEligible = false;
+var losers;
