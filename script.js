2e08e208e0c81a3f76eab91d39696ea8563ffc16
etominskdetka <9798dima@gmail.com>
2021-04-18 22:08:59 +0300

    feat: learn js

--- script.js
+++ script.js
@@ -7,8 +7,119 @@ if (age > 14) {
   alert("Welcome " + name + "!");
 }*/
 
-
-var winners = 2;
+/*var winners = 2;
 var name = "Duke";
 var isEligible = false;
-var losers;
+var losers;*/
+
+//x = x + 1;
+
+//x =    123;
+
+//var width;
+
+//var Counter;
+//var counter;
+
+/*var price = 10;
+var discount = 20;
+var total = price - price * (discount / 100);
+document.write(total);
+console.log("Dear " + "Reader" + ",");
+let a = "ber";
+console.log("super" + "cali" + a);*/
+
+/*let age = 7;
+
+if (age < 14) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let cost = 2.55;
+
+if (cost >= 3.99) {
+  console.log("Истина");
+} else {
+  console.log("Ложь");
+}
+
+let animal = "bear";
+
+if (animal == "bear") {
+  console.log("Да, это медведь");
+} else {
+  console.log("Это не медведь");
+}*/
+
+/*let name = "Martha";
+console.log(name + ", " + "you've won!");
+
+let color = "pink";
+console.log(color != "orange");*/
+
+/*let scoops = 5;
+
+while (scoops > 0) {
+  document.write("Another scoop!</br>");
+  scoops = scoops - 1;
+}
+
+document.write("Life without ice cream is not the same");*/
+
+/*if (scoops < 3) {
+  alert("Ice creame is running low!");
+}*/
+
+/*if (scoops >= 5) {
+  alert("Eat faster");
+} else if (scoops < 3) {
+  alert("Ice creame is running low!");
+}*/
+
+/*if (scoops >= 5) {
+  alert("Еще много мороженного");
+} else if (scoops == 3) {
+  alert("Мороженного еще много");
+} else if (scoops == 2) {
+  alert("Мороженного достаточно");
+} else if (scoops == 1) {
+  alert("Мороженное заканчивается");
+} else if (scoops == 0) {
+  alert("Мороженное закончилось");
+} else {
+  alert("Количество мороженного неизвестно");
+}*/
+
+/*let i = 0;
+let name = prompt("Как Ваше имя?");
+
+while (i < 2) {
+  document.write("Happy Bithday to You</br>");
+  i = i + 1;
+}
+
+document.write(`Happy Bithday, dear ${name} </br>`);
+document.write("Happy Bithday to You");
+*/
+
+//var message = "Howdy" + " " + "partner";
+//console.log(message);
+
+/*let word = "bottles";
+let count = 99;
+
+while(count > 0) {
+  console.log(count + " " + word + " of beer on the wall");
+  console.log(count + " " + word + " of beer,");
+  console.log("Take one down, pass it around");
+  count = count - 1;
+  if (count > 0) {
+    console.log(count + " " + word + " of beer on the wall");
+  } else {
+    console.log("No more " + word + " of beer on the wall");
+  }
+}*/
+
+
